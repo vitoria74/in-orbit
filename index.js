9d551fae6a2ec8d7e21b399bebab354dfba29b24
vitoria74 <maria_vitoria74@hotmail.com>
2024-09-15 18:42:14 -0300

    menu

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const {select} = require('@inquirer/prompts')
+const { select } = require('@inquirer/prompts')
 
-const start = () => {
+const start = async() => {
 
     while(true){
-        let opcao = 'sair'
+
+        const opcao = await select({
+            message: 'Menu >',
+            choices: [
+                {
+                    name: 'Cadastrar meta',
+                    value: 'cadastrar'
+                },
+
+                {
+                    name: 'Listar metas',
+                    value: 'listar'
+                },
+
+                {
+                    name: 'Sair',
+                     value: 'sair'
+                }
+            ],
+        })
+
         switch(opcao){
             case 'cadastrar':
                 console.log('vamos cadastrar')
@@ -12,6 +32,7 @@ const start = () => {
                 console.log('vamos listar')
                 break
             case 'sair':
+                console.log('Até a próxima')
                 return
         }
     }
